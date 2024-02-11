796a7f99ed9e90832491ccb20d5dff15109ca7a3
encoderit-rafi <112605862+encoderit-rafi@users.noreply.github.com>
2024-02-12 00:18:18 +0600

    main

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </div>
+  );
+}
